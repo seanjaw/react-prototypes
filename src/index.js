0fbe692aaa21f291431d0295add034f153c932f6
seanjaw <sjaw94@gmail.com>
2019-02-08 21:26:17 -0800

    completed grerting and name

--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const userObj={ 
+    name: 'Sean',
+    luckyNumber: luckyNumber()
+};
+
+
+function luckyNumber(){
+    return Math.floor(Math.random()*1000 )+1;  
+}
+
+function greeting(userObj){
+
+    return(
+        <div>
+            <h1>
+            Greetings {userObj.name}
+            </h1>
+            <h2 className='text-muted'>The user's number is {userObj.luckyNumber}</h2>
+        </div>
+    );
+}
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    // <h1 className="container">React Prototypes!</h1>,
+    greeting(userObj),
     document.getElementById('root')
 );
